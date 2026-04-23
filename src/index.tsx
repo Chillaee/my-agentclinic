183f6996d16fa0b4bb1c2024c995c295a9f58b2a
Your Name <127109526+Chillaee@users.noreply.github.com>
2026-04-23 17:40:55 +0100

    Add initial thingy

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
+import { Home } from './pages/Home.js'
+
+const app = new Hono()
+
+app.get('/', (c) => c.html(<Home />))
+
+serve({ fetch: app.fetch, port: 3000 }, () => {
+  console.log('AgentClinic running on http://localhost:3000')
+})
